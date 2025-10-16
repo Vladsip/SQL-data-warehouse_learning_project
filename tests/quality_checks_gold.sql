7dcb0898392d797ecff473866031b0ed2cb2e669
Vladimír Šíp <148846331+Vladsip@users.noreply.github.com>
2025-10-16 07:18:25 +0200

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -20,7 +20,7 @@ Usage Notes:
 -- Check for Uniqueness of Customer Key in gold.dim_customers
 -- Expectation: No results   
 
-`SELECT 
+SELECT 
     customer_key,
     COUNT(*) AS duplicate_count
 FROM gold.dim_customers
@@ -32,7 +32,7 @@ HAVING COUNT(*) > 1;`
 -- ====================================================================
 -- Check for Uniqueness of Product Key in gold.dim_products
 -- Expectation: No results 
-`SELECT 
+SELECT 
     product_key,
     COUNT(*) AS duplicate_count
 FROM gold.dim_products
@@ -43,7 +43,7 @@ HAVING COUNT(*) > 1;`
 -- Checking 'gold.fact_sales'
 -- ====================================================================
 -- Check the data model connectivity between fact and dimensions
-`SELECT * 
+SELECT * 
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
 ON c.customer_key = f.customer_key
